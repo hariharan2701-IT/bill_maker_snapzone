84eab761da0adeb621f06903241d6528f04096dc
Hari Haran k <hariharan27012004@gmail.com>
2025-07-08 17:41:30 +0530

    Fix missing JavaScript in build

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html'
-      },
-      external: ['https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js']
+      }
     }
   }
 })
