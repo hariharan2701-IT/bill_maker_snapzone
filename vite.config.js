d125c5c5bc5dbf1b0151a6f2b9c1f7ff64f8bc59
Hari Haran k <hariharan27012004@gmail.com>
2025-07-08 17:39:57 +0530

    Fix deployment by including JavaScript file

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html'
+      },
+      external: ['https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js']
+    }
+  }
+})
